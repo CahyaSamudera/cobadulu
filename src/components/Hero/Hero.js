e4e0e23aa1e6970e692005843448495d404eeb33
CahyaSamudera <cahyasamudera@gmail.com>
2022-02-16 14:01:53 +0700

    add a href

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
           <h1>Jasa Penerjemah</h1>
           <p>Jasa Penerjemah Resmi dan Tersumpah</p>
           <div className='btn-group'>
-            <a>
+            <a href='https://sucayono.com/'>
               <Link to='contact' className='color'>
                 Hubungi Kami
               </Link>
